aadcceda75bca81473c32a774124600d415e14c4
viktor <viktor.netology@gmail.com>
2023-04-25 09:11:43 +0300

    add file

---
+++ main.py
@@ -0,0 +1,76 @@
+# 1.
+class FlatIterator:
+    def __init__(self, list_of_lists):
+        self.flat_list = []
+        self.flatten(list_of_lists)
+
+    def flatten(self, lst):
+        for item in lst:
+            if type(item) == list:
+                self.flatten(item)
+            else:
+                self.flat_list.append(item)
+
+    def __iter__(self):
+        self.index = 0
+        return self
+
+    def __next__(self):
+        if self.index < len(self.flat_list):
+            item = self.flat_list[self.index]
+            self.index += 1
+            return item
+        else:
+            raise StopIteration
+
+
+def test_1():
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+if __name__ == '__main__':
+    test_1()
+
+# 2
+import types
+
+def flat_generator(list_of_lists):
+    for elem in list_of_lists:
+        if isinstance(elem, list):
+            yield from flat_generator(elem)
+        else:
+            yield elem
+
+def test_2():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+if __name__ == '__main__':
+    test_2()
+
